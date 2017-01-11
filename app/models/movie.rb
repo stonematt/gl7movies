d454556dc2c0ca4a72d641be51921b42ab90a2e2
stone <mstone@stonecover.com>
2017-01-11 10:24:43 -0700

    added search by genre

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,6 +5,7 @@ class Movie < ApplicationRecord
 
   scope :title, -> (title) { where("title like ?", "%#{title}%") }
   scope :rating, -> (rating) { where("rating like ?", "#{rating}") }
+  scope :genre, -> (genre) { where("genre like ?", "#{genre}") }
   scope :lead, -> (lead) { where("lead like ?", "%#{lead}%") }
   scope :director, -> (director) { where("director like ?", "%#{director}%") }
   scope :medium, -> (medium) { where("medium like ?", "%#{medium}%") }
