ecc32f9bbbc73de9dde7bf5d33f6aa84dfe110bb
stone <mstone@stonecover.com>
2017-01-10 17:31:27 -0700

    search by explicit rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,8 +1,10 @@
 class Movie < ApplicationRecord
   include Filterable
+  
+  default_scope { order('title') } 
 
   scope :title, -> (title) { where("title like ?", "%#{title}%") }
-  scope :rating, -> (rating) { where("rating like ?", "%#{rating}%") }
+  scope :rating, -> (rating) { where("rating like ?", "#{rating}") }
   scope :lead, -> (lead) { where("lead like ?", "%#{lead}%") }
   scope :director, -> (director) { where("director like ?", "%#{director}%") }
   scope :medium, -> (medium) { where("medium like ?", "%#{medium}%") }
