688b9331f1585dc14a8f182d2df081a44bc7db37
stone <mstone@stonecover.com>
2017-01-11 09:51:07 -0700

    added column order

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,10 +9,12 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+//=
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
 //= require tether
 //= require bootstrap-sprockets
+//= require moment
+//= require bootstrap-sortable
 //= require_tree .
